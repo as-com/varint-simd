0678cba70ac9474b8dd499871a685ff9efdc3fd9
Andrew Sun <me@andrewsun.com>
2020-12-26 03:52:16 -0500

    Add prost-varint comparison

---
+++ benches/prost_varint.rs
@@ -0,0 +1,132 @@
+use std::cmp::min;
+use bytes::Buf;
+
+/// Decodes a LEB128-encoded variable length integer from the buffer.
+pub fn decode_varint<B>(buf: &mut B) -> Result<u64, ()>
+    where
+        B: Buf,
+{
+    let bytes = buf.chunk();
+    let len = bytes.len();
+    if len == 0 {
+        return Err(());
+    }
+
+    let byte = unsafe { *bytes.get_unchecked(0) };
+    if byte < 0x80 {
+        buf.advance(1);
+        Ok(u64::from(byte))
+    } else if len > 10 || bytes[len - 1] < 0x80 {
+        let (value, advance) = unsafe { decode_varint_slice(bytes) }?;
+        buf.advance(advance);
+        Ok(value)
+    } else {
+        decode_varint_slow(buf)
+    }
+}
+
+/// Decodes a LEB128-encoded variable length integer from the slice, returning the value and the
+/// number of bytes read.
+///
+/// Based loosely on [`ReadVarint64FromArray`][1].
+///
+/// ## Safety
+///
+/// The caller must ensure that `bytes` is non-empty and either `bytes.len() >= 10` or the last
+/// element in bytes is < `0x80`.
+///
+/// [1]: https://github.com/google/protobuf/blob/3.3.x/src/google/protobuf/io/coded_stream.cc#L365-L406
+#[inline]
+unsafe fn decode_varint_slice(bytes: &[u8]) -> Result<(u64, usize), ()> {
+    // Fully unrolled varint decoding loop. Splitting into 32-bit pieces gives better performance.
+
+    let mut b: u8;
+    let mut part0: u32;
+    b = *bytes.get_unchecked(0);
+    part0 = u32::from(b);
+    if b < 0x80 {
+        return Ok((u64::from(part0), 1));
+    };
+    part0 -= 0x80;
+    b = *bytes.get_unchecked(1);
+    part0 += u32::from(b) << 7;
+    if b < 0x80 {
+        return Ok((u64::from(part0), 2));
+    };
+    part0 -= 0x80 << 7;
+    b = *bytes.get_unchecked(2);
+    part0 += u32::from(b) << 14;
+    if b < 0x80 {
+        return Ok((u64::from(part0), 3));
+    };
+    part0 -= 0x80 << 14;
+    b = *bytes.get_unchecked(3);
+    part0 += u32::from(b) << 21;
+    if b < 0x80 {
+        return Ok((u64::from(part0), 4));
+    };
+    part0 -= 0x80 << 21;
+    let value = u64::from(part0);
+
+    let mut part1: u32;
+    b = *bytes.get_unchecked(4);
+    part1 = u32::from(b);
+    if b < 0x80 {
+        return Ok((value + (u64::from(part1) << 28), 5));
+    };
+    part1 -= 0x80;
+    b = *bytes.get_unchecked(5);
+    part1 += u32::from(b) << 7;
+    if b < 0x80 {
+        return Ok((value + (u64::from(part1) << 28), 6));
+    };
+    part1 -= 0x80 << 7;
+    b = *bytes.get_unchecked(6);
+    part1 += u32::from(b) << 14;
+    if b < 0x80 {
+        return Ok((value + (u64::from(part1) << 28), 7));
+    };
+    part1 -= 0x80 << 14;
+    b = *bytes.get_unchecked(7);
+    part1 += u32::from(b) << 21;
+    if b < 0x80 {
+        return Ok((value + (u64::from(part1) << 28), 8));
+    };
+    part1 -= 0x80 << 21;
+    let value = value + ((u64::from(part1)) << 28);
+
+    let mut part2: u32;
+    b = *bytes.get_unchecked(8);
+    part2 = u32::from(b);
+    if b < 0x80 {
+        return Ok((value + (u64::from(part2) << 56), 9));
+    };
+    part2 -= 0x80;
+    b = *bytes.get_unchecked(9);
+    part2 += u32::from(b) << 7;
+    if b < 0x80 {
+        return Ok((value + (u64::from(part2) << 56), 10));
+    };
+
+    // We have overrun the maximum size of a varint (10 bytes). Assume the data is corrupt.
+    Err(())
+}
+
+/// Decodes a LEB128-encoded variable length integer from the buffer, advancing the buffer as
+/// necessary.
+#[inline(never)]
+fn decode_varint_slow<B>(buf: &mut B) -> Result<u64, ()>
+    where
+        B: Buf,
+{
+    let mut value = 0;
+    for count in 0..min(10, buf.remaining()) {
+        let byte = buf.get_u8();
+        value |= u64::from(byte & 0x7F) << (count * 7);
+        if byte <= 0x7F {
+            return Ok(value);
+        }
+    }
+
+    Err(())
+}
--- benches/varint_bench.rs
+++ benches/varint_bench.rs
@@ -2,15 +2,19 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion, BatchSize
 use integer_encoding::VarInt;
 use varint_simd::decode_unsafe;
 use rand::{thread_rng, Rng};
+use bytes::Buf;
+
+mod prost_varint;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    let my_u64 = 4294967295;
+    let my_u32 = 4294967295;
 
     let mut encoded = [0;16];
-    black_box(my_u64).encode_var(&mut encoded);
+    black_box(my_u32).encode_var(&mut encoded);
 
-    assert_eq!(my_u64, u32::decode_var(&encoded).unwrap().0);
-    assert_eq!(my_u64, unsafe { decode_unsafe::<u32>(&encoded).0 });
+    assert_eq!(my_u32, u32::decode_var(&encoded).unwrap().0);
+    assert_eq!(my_u32, unsafe { decode_unsafe::<u32>(&encoded).0 });
+    assert_eq!(my_u32 as u64, prost_varint::decode_varint(&mut encoded.to_vec().as_slice()).unwrap());
 
     let mut rng = thread_rng();
 
@@ -21,7 +25,17 @@ pub fn criterion_benchmark(c: &mut Criterion) {
             rng.gen::<u8>().encode_var(&mut encoded);
             encoded
         }, |encoded| {
-            u64::decode_var(black_box(&encoded)).unwrap().0
+            u8::decode_var(black_box(&encoded)).unwrap().0
+        }, BatchSize::SmallInput)
+    });
+
+    group.bench_function("prost-varint", |b| {
+        b.iter_batched(|| {
+            let mut encoded = [0;16];
+            rng.gen::<u8>().encode_var(&mut encoded);
+            encoded.to_vec()
+        }, |mut encoded| {
+            prost_varint::decode_varint(black_box(&mut encoded.as_slice())).unwrap()
         }, BatchSize::SmallInput)
     });
 
@@ -43,7 +57,17 @@ pub fn criterion_benchmark(c: &mut Criterion) {
             rng.gen::<u16>().encode_var(&mut encoded);
             encoded
         }, |encoded| {
-            u64::decode_var(black_box(&encoded)).unwrap().0
+            u16::decode_var(black_box(&encoded)).unwrap().0
+        }, BatchSize::SmallInput)
+    });
+
+    group.bench_function("prost-varint", |b| {
+        b.iter_batched(|| {
+            let mut encoded = [0;16];
+            rng.gen::<u16>().encode_var(&mut encoded);
+            encoded.to_vec()
+        }, |mut encoded| {
+            prost_varint::decode_varint(black_box(&mut encoded.as_slice())).unwrap()
         }, BatchSize::SmallInput)
     });
 
@@ -69,6 +93,16 @@ pub fn criterion_benchmark(c: &mut Criterion) {
         }, BatchSize::SmallInput)
     });
 
+    group.bench_function("prost-varint", |b| {
+        b.iter_batched(|| {
+            let mut encoded = [0;16];
+            rng.gen::<u32>().encode_var(&mut encoded);
+            encoded.to_vec()
+        }, |mut encoded| {
+            prost_varint::decode_varint(black_box(&mut encoded.as_slice())).unwrap()
+        }, BatchSize::SmallInput)
+    });
+
     group.bench_function("varint-simd", |b| {
         b.iter_batched(|| {
             let mut encoded = [0;16];
@@ -88,6 +122,16 @@ pub fn criterion_benchmark(c: &mut Criterion) {
             encoded
         }, |encoded| {
             u64::decode_var(black_box(&encoded)).unwrap().0
+        }, BatchSize::SmallInput)
+    });
+
+    group.bench_function("prost-varint", |b| {
+        b.iter_batched(|| {
+            let mut encoded = [0;16];
+            rng.gen::<u64>().encode_var(&mut encoded);
+            encoded.to_vec()
+        }, |mut encoded| {
+            prost_varint::decode_varint(black_box(&mut encoded.as_slice())).unwrap()
         }, BatchSize::SmallInput)
     });
 
